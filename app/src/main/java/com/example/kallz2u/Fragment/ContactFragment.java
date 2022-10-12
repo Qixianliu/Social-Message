be82ec609fc45d74daa8935fe60109f8b6b10622
Markabaka <s3798766@student.rmit.edu.au>
2022-10-13 06:07:39 +1100

    display contact list

--- app/src/main/java/com/example/kallz2u/Fragment/ContactFragment.java
+++ app/src/main/java/com/example/kallz2u/Fragment/ContactFragment.java
@@ -1,104 +1,125 @@
 
 package com.example.kallz2u.Fragment;
 
-import android.content.ContentResolver;
-import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.example.kallz2u.Permissions.Permissions;
 import com.example.kallz2u.R;
-import com.example.kallz2u.databinding.FragmentContactBinding;
+import com.example.kallz2u.bean.User;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContactFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ContactFragment extends Fragment {
 
-    private FragmentContactBinding binding;
-    private DatabaseReference databaseReference;
-    private Permissions permissions;
+    private View ContactsView;
+    private RecyclerView myContactsList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private DatabaseReference databaseReference, UsersRef;
+    private RecyclerView recyclerView;
+    private FirebaseAuth firebaseAuth;
+    private String currentUserId;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public ContactFragment(){
 
-    public ContactFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContactFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContactFragment newInstance(String param1, String param2) {
-        ContactFragment fragment = new ContactFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ContactsView = inflater.inflate(R.layout.fragment_contact,container,false);
+
+        myContactsList = ContactsView.findViewById(R.id.usersRecyclerView);
+        myContactsList.setLayoutManager(new LinearLayoutManager(ContactsView.getContext()));
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("users");
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUserId = firebaseAuth.getCurrentUser().getUid();
+
+
+
         // Inflate the layout for this fragment
-        binding = FragmentContactBinding.inflate(inflater,container,false);
-        View view = binding.getRoot();
-        permissions= new Permissions();
-        return view;
+        return ContactsView;
     }
 
-    private void getUserContact(){
-        if(permissions.isContactOk(getContext())){
-            String[] projection = new String[]{
-                    /*ContactsContract.Contacts.DISPLAY_NAME,
-                    ContactsContract.CommonDataKinds.Email.*/
-            };
+    @Override
+    public void onStart() {
 
-            ContentResolver cr = getActivity().getContentResolver();
-            Cursor cursor=cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,projection,null,null,null);
-            if(cursor != null){
-                while(cursor.moveToFirst()){
-                    /*String name=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DISPLAY_NAME));
-                    String email=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DISPLAY_EMAIL);*/
+        super.onStart();
 
-                }
+        FirebaseRecyclerOptions<User> options =
+                new FirebaseRecyclerOptions.Builder<User>()
+                        .setQuery(databaseReference,User.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<User,ContactsViewHolder> adapter
+                = new FirebaseRecyclerAdapter<User, ContactsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ContactsViewHolder holder, int position, @NonNull User model) {
+                String userIDs = getRef(position).getKey();
+                UsersRef.child(userIDs).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        holder.username.setText(model.getUsername());
+                        holder.email.setText(model.getEmail());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
-        }else{
 
-        }
+            @NonNull
+            @Override
+            public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_item_layout,parent,false);
+                ContactsViewHolder viewHolder = new ContactsViewHolder(view);
+                return viewHolder;
+            }
+        };
+
+        myContactsList.setAdapter(adapter);
+        adapter.startListening();
 
     }
+
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder{
+
+        TextView username, email;
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            username = itemView.findViewById(R.id.txtContactName);
+            email = itemView.findViewById(R.id.txtContactEmail);
+        }
+    }
+
+
+
+
+
+
 }
 
     /*private FragmentContactBinding binding;
--- app/src/main/java/com/example/kallz2u/bean/User.java
+++ app/src/main/java/com/example/kallz2u/bean/User.java
@@ -13,6 +13,22 @@ public class User {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     public User(String username, String email) {
         this.username = username;
         this.email = email;
