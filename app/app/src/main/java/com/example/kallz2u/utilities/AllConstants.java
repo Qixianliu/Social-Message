e4918a8ebb7cba8792365cb7390560bc6bf0a615
s3839767 <80165093+s3839767@users.noreply.github.com>
2022-11-01 03:26:26 +1100

    changed structure of project

--- app/app/src/main/java/com/example/kallz2u/utilities/AllConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.kallz2u.utilities;
-
-public interface AllConstants {
-    String VERIFICATION_CODE = "code";
-    String IMAGE_PATH = "?";
-    int STORAGE_REQUEST_CODE = 1000;
-    int USERNAME_CODE = 100;
-    int CONTACTS_REQUEST_CODE = 2000;
-}
