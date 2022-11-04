d7e4e3103074ddc81bc40ce0cc3072011a4898dc
QixianLiu(s3706396) <87603153+Qixianliu@users.noreply.github.com>
2022-11-04 16:12:51 +0800

    Update App.java

--- app/src/main/java/com/example/kallz2u/App.java
+++ app/src/main/java/com/example/kallz2u/App.java
@@ -1,12 +1,6 @@
 package com.example.kallz2u;
 
 import org.litepal.LitePalApplication;
-/**
- * @Author : qixian
- * @Email : 827112947@qq.com
- * @Date : on 2022-11-03 09:51.
- * @Description :描述
- */
 
 public class App extends LitePalApplication {
     @Override
